99c58cb8c69d9de3ab438ee6b675cb756880c8e0
john <johnmaluki58@gmail.com>
2019-08-01 17:46:57 +0300

    changed blog url to reflect home path

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ from .views import (
 app_name = 'blog'
 
 urlpatterns = [
-    path('blog/', BlogListView.as_view(), name='home'),
+    path('', BlogListView.as_view(), name='home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
