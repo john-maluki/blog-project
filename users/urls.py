82c2d2651163a62ab3140c1b28b30f18638f47b2
john <johnmaluki58@gmail.com>
2019-08-09 16:58:40 +0300

    google login enabled

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import SignUpView
+from .views import SignUpView, loginView
 
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', loginView,  name='login'),
     path('auth/', include('social_django.urls', namespace='social')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
@@ -7,3 +8,7 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/signup.html'
+
+
+def loginView(request):
+    return render(request, 'registration/login.html')
